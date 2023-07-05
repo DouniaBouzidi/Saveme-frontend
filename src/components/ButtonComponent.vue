671fd52e367b2dd3ba8f55b5320d2aa3fba71f26
Dounia Bouzidi <dani-12889@outlook.fr>
2023-07-05 20:06:07 +0200

    Added Button Component and Not Found

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>
+    Button
+  </button>
+</template>
+<script>
+export default {
+  name: 'ButtonComponent'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="notfound">
+    <h1>404 Not Found Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundView'
+}
+</script>
+
+<style scoped>
+</style>
