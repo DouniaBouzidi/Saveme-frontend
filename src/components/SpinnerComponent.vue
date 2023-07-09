72137076fa2680d6d3fd06c69c28fd5a513130ee
Dounia Bouzidi <dani-12889@outlook.fr>
2023-07-09 23:28:05 +0200

    Created SpinnerComponent

---
+++ src/components/SpinnerComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center">
+    <font-awesome-icon icon="fa-solid fa-fan" style="color: var(--important-color)" size="3x" spin/>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'SpinnerComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
