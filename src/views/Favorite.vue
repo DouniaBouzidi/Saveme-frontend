4b287febf9caa489e31e671525beeeb5588f3180
Dounia Bouzidi <dani-12889@outlook.fr>
2023-07-09 23:33:45 +0200

    add FavoriteSongs

---
+++ src/views/Favorite.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1 className="page-header">Click to get <span style="color: #D41850 ">your</span> favorite song 😎</h1>
+  <div className="d-flex justify-content-center align-items-center">
+    <img className="banner-image" alt="Vue logo" src="../assets/logo.png">
+    <div className="random-button">
+      <font-awesome-icon icon="fa-solid fa-circle-play" className="play-icon" beat size="10x"
+                         style="--fa-animation-duration: 2.5s; --fa-beat-scale: 1.2;"
+                         @click="$router.push({path: '/favorite-song'})"/>
+    </div>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'FavoriteSongs'
+}
+</script>
+
+<style scoped>
+
+.banner-image {
+  max-width: 70%;
+  min-height: 200px;
+  max-height: 700px;
+  border-radius: 20px;
+  margin: 100px;
+}
+
+.random-button {
+  position: absolute;
+  margin: 0 auto;
+  text-align: center
+}
+
+.play-icon {
+  color: var(--primary-color);
+}
+
+.play-icon:hover {
+  color: var(--warning-color);
+}
+</style>
---
+++ src/views/FavoriteSong.vue
@@ -0,0 +1,149 @@
+<template>
+  <h1 class="page-header">Your <span style="color: #D41850">songs</span></h1>
+
+  <div class="container-fluid">
+    <spinner-component v-show="showSpinner"/>
+    <div class="row row-cols-auto gap-5 d-flex justify-content-center px-5">
+      <div class="col-sm" v-show="!showSpinner">
+        <div class="card mb-5 info-card p-5 shadow ">
+          <h5 class="list-header mb-5 text-center" style="color: #D41850">Favorites</h5>
+          <ul class="list-group overflow-scroll">
+            <li v-for="song in favoriteSongs" :key="song.id">
+              <FavoriteListItemComponent :song="song" @showFavoriteSongEvent="showFavoriteSong"/>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-sm favorite-song" v-show="!showSpinner">
+        <div class="mb-4">
+          <button v-show="!showHeart" class="button-util button-heart shadow m-2"
+                  @click="setFavoriteState(this.songs[0].id, true)">
+            <font-awesome-icon icon="fa-solid fa-heart" size="2x"/>
+          </button>
+          <button v-show="showHeart" class="button-util button-heart shadow m-2"
+                  @click="setFavoriteState(this.songs[0].id, false)">
+            <font-awesome-icon icon="fa-solid fa-heart-crack" size="2x"/>
+          </button>
+        </div>
+        <div class="col-sm" v-for="song in songs" :key="song.id">
+          <song-card :song="song" :remove-button-available="false"></song-card>
+        </div>
+      </div>
+      <div class="col-sm" v-show="!showSpinner">
+        <guide-card-component/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SongCard from '@/components/SongCardComponent'
+import GuideCardComponent from '@/components/GuideCardComponent'
+import FavoriteListItemComponent from '@/components/FavoriteListItemComponent'
+import SpinnerComponent from '@/components/SpinnerComponent'
+
+export default {
+  name: 'FavoriteSong',
+  emits: ['showFavoriteSongEvent'],
+  components: {
+    SpinnerComponent,
+    FavoriteListItemComponent,
+    GuideCardComponent,
+    SongCard
+  },
+  data () {
+    return {
+      songs: [],
+      favoriteSongs: [],
+      showHeart: true,
+      showSpinner: true
+    }
+  },
+  mounted () {
+    this.fetchFavoriteSongs()
+    this.fetchFavoriteSongs()
+    setTimeout(this.showContent, 300)
+  },
+  methods: {
+    showContent () {
+      this.showSpinner = false
+    },
+    async fetchFavoriteSongs () {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/songs/favorites'
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      }
+      await fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(result => result.forEach(song => {
+          this.favoriteSongs.push(song)
+        }))
+        .catch(error => console.log('error', error))
+    },
+    showFavoriteSong (songId) {
+      this.songs[0] = this.favoriteSongs[this.favoriteSongs.findIndex((song) => song.id === songId)]
+      this.showHeart = this.songs[0].isFavorite
+    },
+    async setFavoriteState (songId, state) {
+      const myHeaders = new Headers()
+      myHeaders.append('Content-Type', 'application/json')
+
+      const raw = JSON.stringify({
+        isFavorite: state
+      })
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/songs/favorites/' + songId
+      const requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      }
+
+      await fetch(endpoint, requestOptions)
+        .catch(error => console.log('error', error))
+
+      if (state && !this.favoriteSongs.some(song => song.id === songId)) {
+        this.favoriteSongs.push({
+          id: songId,
+          title: this.songs[0].title,
+          author: this.songs[0].author,
+          releaseYear: this.songs[0].releaseYear,
+          songLink: this.songs[0].songLink,
+          isFavorite: true
+        })
+        this.songs[0].isFavorite = true
+        this.showHeart = true
+        this.$notify({
+          type: 'success',
+          text: 'You have added this song to your favorites'
+        })
+      } else if (!state && this.favoriteSongs.some(song => song.id === songId)) {
+        this.favoriteSongs.splice(this.favoriteSongs.findIndex((song) => song.id === songId), 1)
+        this.songs[0].isFavorite = false
+        this.showHeart = false
+        this.$notify({
+          type: 'error',
+          text: 'You have removed the song from your favorites'
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container-fluid {
+  background-color: var(--background-color);
+}
+
+.random-song {
+  text-align: -webkit-center;
+}
+
+.list-group {
+  height: 450px;
+  min-width: 300px;
+}
+
+</style>
